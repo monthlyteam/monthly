9ee2dafbb69ac99a01aae5c3a450b428a5c1e0f9
renumber <jwook9331@naver.com>
2020-07-06 14:54:21 +0900

    test1

--- monthly/lib/main.dart
+++ monthly/lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(),
     );
   }
 }
@@ -19,6 +19,8 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.teal,
+    );
   }
 }
