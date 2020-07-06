0a465138b3b56b7826316f445585da4596b6dc74
renumber <jwook9331@naver.com>
2020-07-06 15:09:17 +0900

    make appbar

--- monthly/lib/main.dart
+++ monthly/lib/main.dart
@@ -20,6 +20,9 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.teal,
+      ),
       backgroundColor: Colors.white,
     );
   }
