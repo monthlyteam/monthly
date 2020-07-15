a658bc593d666c59824b0720197c77df5e19b3c6
허석민 <gjtjrals0@gmail.com>
2020-07-15 21:56:23 +0900

    Add ColorList for Stock's color

--- monthly/lib/constants.dart
+++ monthly/lib/constants.dart
@@ -2,3 +2,26 @@ import 'package:flutter/material.dart';
 
 const kTextColor = Color(0xff2C2C2C);
 const kMainColor = Color(0xff19a2b5);
+
+const kColorList = [
+  Color(0xFFF09797),
+  Color(0xFFE8B447),
+  Color(0xFF1FC49B),
+  Color(0xFFC5CC2A),
+  Color(0xFF1EBBDD),
+  Color(0xFFF25B7F),
+  Color(0xFFB98E1E),
+  Color(0xFF18A27E),
+  Color(0xFF88B14B),
+  Color(0xFF538FE0),
+  Color(0xFFCC3A3E),
+  Color(0xFFB77C1F),
+  Color(0xFF0E7961),
+  Color(0xFF599318),
+  Color(0xFF7258FC),
+  Color(0xFFAC2727),
+  Color(0xFF9F650A),
+  Color(0xFF9B9B9B),
+  Color(0xFF386A0E),
+  Color(0xFF2C206C)
+];
