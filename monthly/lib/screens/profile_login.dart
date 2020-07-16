7a6a1b1de82a1f9f294875a11ffdfdb56c91f6ad
허석민 <gjtjrals0@gmail.com>
2020-07-16 17:03:59 +0900

    input Kakao SDK in pubspec.yaml

--- monthly/lib/screens/profile_login.dart
+++ monthly/lib/screens/profile_login.dart
@@ -1,38 +1,57 @@
 import 'package:flutter/material.dart';
 import 'package:monthly/constants.dart';
 
-class ProfileLogin extends StatelessWidget {
+class ProfileLogin extends StatefulWidget {
+  @override
+  _ProfileLoginState createState() => _ProfileLoginState();
+}
+
+class _ProfileLoginState extends State<ProfileLogin> {
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: CustomScrollView(
-        slivers: <Widget>[
-          SliverAppBar(
-            titleSpacing: 0.0,
-            elevation: 0.0,
-            title: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 25.0),
-              child: Text(
-                "로그인",
-                style: TextStyle(
-                    color: kTextColor,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 20),
-              ),
-            ),
-            leading: IconButton(
-                icon: Icon(
-                  Icons.navigate_before,
-                  color: kTextColor,
-                  size: 30,
+    return Scaffold(
+      body: SafeArea(
+        child: CustomScrollView(
+          slivers: <Widget>[
+            SliverAppBar(
+              titleSpacing: 0.0,
+              elevation: 0.0,
+              title: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                child: Text(
+                  "로그인",
+                  style: TextStyle(
+                      color: kTextColor,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20),
                 ),
-                onPressed: () {
-                  Navigator.pop(context);
-                }),
-            floating: true,
-            backgroundColor: Colors.white,
-          ),
-        ],
+              ),
+              leading: IconButton(
+                  icon: Icon(
+                    Icons.navigate_before,
+                    color: kTextColor,
+                    size: 30,
+                  ),
+                  onPressed: () {
+                    Navigator.pop(context);
+                  }),
+              floating: true,
+              backgroundColor: Colors.white,
+            ),
+            SliverFillRemaining(
+              hasScrollBody: false,
+              child: Center(
+                child: Container(
+                  color: Colors.yellow,
+                  child: FlatButton(
+                    onPressed: () {},
+                    child: Text("KaKaoLogin"),
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
