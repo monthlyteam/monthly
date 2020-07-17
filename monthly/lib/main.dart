aef2eb51b8c0a44184e37c90072168d841eac084
renumber <jwook9331@naver.com>
2020-07-17 18:56:44 +0900

    change container to roundrectborder

--- monthly/lib/main.dart
+++ monthly/lib/main.dart
@@ -24,7 +24,6 @@ class MyApp extends StatelessWidget {
       title: 'Monthly',
       theme: ThemeData(
         fontFamily: 'NanumGothic',
-        canvasColor: Colors.transparent,
       ),
       home: Home(),
     );
