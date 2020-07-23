bb673efbb581128765ee00139759287a23c2626b
허석민 <gjtjrals0@gmail.com>
2020-07-23 14:20:06 +0900

    Make KakaoLogin react page, Add question mail service, Add url_launcher in pubspcet.yaml for mail service

--- monthly/lib/screens/profile.dart
+++ monthly/lib/screens/profile.dart
@@ -1,10 +1,115 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:monthly/constants.dart';
-import 'profile_login.dart';
-import 'profile_question.dart';
 import 'profile_settings.dart';
+import 'package:kakao_flutter_sdk/all.dart';
+import 'package:url_launcher/url_launcher.dart';
 
-class Profile extends StatelessWidget {
+class Profile extends StatefulWidget {
+  @override
+  _ProfileState createState() => _ProfileState();
+}
+
+class _ProfileState extends State<Profile> {
+  bool _isKakao = false;
+  bool _isKakaoLogin = false;
+  String _userName = '사용자';
+
+  @override
+  void initState() {
+    super.initState();
+    _isKakaoInstalled();
+  }
+
+  _launchURL() async {
+    final Uri params = Uri(
+      scheme: 'mailto',
+      path: 'gjtjrals0@gmail.com',
+      query: 'subject=먼슬리 문의 메일 &body=문의 내용을 아래에 써서 보내주시면 최대한 빠르게 응답하겠습니다!\n >',
+    );
+
+    var url = params.toString();
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
+
+  void _isKakaoInstalled() async {
+    final installed = await isKakaoTalkInstalled();
+    setState(() {
+      _isKakao = installed;
+    });
+  }
+
+  _accessToken(String authCode) async {
+    try {
+      var token = await AuthApi.instance.issueAccessToken(authCode);
+      AccessTokenStore.instance.toStore(token);
+      _getUserKakaoName();
+      setState(() {
+        _isKakaoLogin = true;
+      });
+      print('token : ${token.toString()} !');
+    } catch (e) {
+      print("accessToken Method Error : $e");
+    }
+  }
+
+  _loginInstalled() async {
+    try {
+      var code = await AuthCodeClient.instance.requestWithTalk();
+      await _accessToken(code);
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  _loginUninstalled() async {
+    try {
+      var code = await AuthCodeClient.instance.request();
+      await _accessToken(code);
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  _logout() async {
+    try {
+      var code = await UserApi.instance.logout();
+      print(code.toString());
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  _unlink() async {
+    try {
+      var code = await UserApi.instance.unlink();
+      setState(() {
+        _isKakaoLogin = false;
+      });
+      print(code.toString());
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  _getUserKakaoName() async {
+    try {
+      User user = await UserApi.instance.me();
+      setState(() {
+        _userName = user.properties['nickname'];
+      });
+    } catch (e) {
+      setState(() {
+        _isKakaoLogin = false;
+      });
+      print('UserData method Error : $e');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -39,34 +144,63 @@ class Profile extends StatelessWidget {
                     color: kTextColor.withOpacity(0.7),
                     size: 110.0,
                   ),
-                  FlatButton(
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => ProfileLogin()),
-                      );
-                    },
-                    child: Container(
-                      width: 140,
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: <Widget>[
-                          Text(
-                            "지금 먼슬리 로그인",
-                            style: TextStyle(
-                                color: kTextColor.withOpacity(0.7),
-                                fontWeight: FontWeight.w700,
-                                fontSize: 14),
+                  _isKakaoLogin
+                      ? Container(
+                          width: 300,
+                          height: 50,
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              Text(
+                                "안녕하세요 ",
+                                style: TextStyle(
+                                    color: kTextColor.withOpacity(0.7),
+                                    fontWeight: FontWeight.w700,
+                                    fontSize: 14),
+                              ),
+                              Text(
+                                "$_userName",
+                                style: TextStyle(
+                                    color: kTextColor.withOpacity(0.7),
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 17),
+                              ),
+                              Text(
+                                "님!",
+                                style: TextStyle(
+                                    color: kTextColor.withOpacity(0.7),
+                                    fontWeight: FontWeight.w700,
+                                    fontSize: 14),
+                              ),
+                            ],
                           ),
-                          Icon(
-                            Icons.navigate_next,
-                            color: kTextColor.withOpacity(0.7),
-                            size: 25.0,
+                        )
+                      : FlatButton(
+                          onPressed: () {
+                            _isKakao ? _loginInstalled() : _loginUninstalled();
+                          },
+                          child: Container(
+                            width: 300,
+                            height: 50,
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: <Widget>[
+                                Text(
+                                  "카카오 아이디로 먼슬리 로그인!",
+                                  style: TextStyle(
+                                      color: kTextColor.withOpacity(0.7),
+                                      fontWeight: FontWeight.w700,
+                                      fontSize: 14),
+                                ),
+                                Icon(
+                                  Icons.navigate_next,
+                                  color: kTextColor.withOpacity(0.7),
+                                  size: 25.0,
+                                ),
+                              ],
+                            ),
                           ),
-                        ],
-                      ),
-                    ),
-                  ),
+                        ),
                 ],
               ),
               height: 250.0,
@@ -121,11 +255,7 @@ class Profile extends StatelessWidget {
                       color: Colors.transparent,
                       child: InkWell(
                         onTap: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => ProfileQuestion()),
-                          );
+                          _launchURL();
                         },
                         child: Container(
                           width: MediaQuery.of(context).size.width,
@@ -144,7 +274,7 @@ class Profile extends StatelessWidget {
                                 width: 15,
                               ),
                               Text(
-                                "문의",
+                                "문의메일",
                                 style: TextStyle(
                                   fontSize: 14,
                                   color: Colors.white,
@@ -154,7 +284,43 @@ class Profile extends StatelessWidget {
                           ),
                         ),
                       ),
-                    )
+                    ),
+                    _isKakaoLogin
+                        ? Material(
+                            color: Colors.transparent,
+                            child: InkWell(
+                              onTap: () {
+                                _unlink();
+                              },
+                              child: Container(
+                                width: MediaQuery.of(context).size.width,
+                                padding: EdgeInsets.all(12.0),
+                                child: Row(
+                                  children: <Widget>[
+                                    SizedBox(
+                                      width: 15,
+                                    ),
+                                    SvgPicture.asset(
+                                      'icons/logout.svg',
+                                      height: 20.0,
+                                      color: Colors.white,
+                                    ),
+                                    SizedBox(
+                                      width: 15,
+                                    ),
+                                    Text(
+                                      "로그아웃",
+                                      style: TextStyle(
+                                        fontSize: 14,
+                                        color: Colors.white,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          )
+                        : Container(),
                   ],
                 ),
               ),
