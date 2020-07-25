db00fecbc48c106607ca7d920a14af735499cdcd
허석민 <gjtjrals0@gmail.com>
2020-07-25 17:19:50 +0900

    change mail adress

--- monthly/lib/screens/profile.dart
+++ monthly/lib/screens/profile.dart
@@ -25,7 +25,7 @@ class _ProfileState extends State<Profile> {
   _launchURL() async {
     final Uri params = Uri(
       scheme: 'mailto',
-      path: 'gjtjrals0@gmail.com',
+      path: 'youngthly@gmail.com',
       query: 'subject=먼슬리 문의 메일 &body=문의 내용을 아래에 써서 보내주시면 최대한 빠르게 응답하겠습니다!\n >',
     );
 
